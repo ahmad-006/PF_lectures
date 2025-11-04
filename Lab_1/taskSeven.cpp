a9ce49800652d063e1680e4b6ce03a816ae55501
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-11-04 09:24:51 +0500

    feat: did the task 7

---
+++ Lab_1/taskSeven.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 10;
+    int b = 5;
+    int c = 2;
+
+    int resultOne = a + b *c;
+    int resultTwo = (a + b) * c;
+
+    cout << "The Answer of 10 + 5 * 2"  << resultOne << endl;
+    cout << "The Answer of (10 + 5) * 2 "<< resultTwo << endl;
+
+    return 0;
+}
