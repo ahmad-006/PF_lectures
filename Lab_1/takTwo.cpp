0fc662f602b32be7f3a72d659c035412c10aaff6
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-11-04 09:08:56 +0500

    feat: did the task 2

---
+++ Lab_1/takTwo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout  << "Name: Ahmad Aamir" << endl;
+    cout  << "Class: ADP Sem - I" << endl;
+    cout  << "Roll No: 18-002" << endl;
+
+    return 0;
+}
