6126d4feace4f34e411aa58c1a57d87fd07ebcd5
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-11-04 09:10:56 +0500

    feat: did the task 5 and 6

---
+++ Lab_1/taskFive.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Learning to code is an \"essential\" skill" << endl;
+    return 0;}
---
+++ Lab_1/taskSix.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 20;
+    int b = 5;
+
+    cout << "The sum is : " << a + b<< endl;
+    cout << "The Difference is : " << a - b<< endl;
+    cout << "The Product is : " << a * b<< endl;
+    cout << "The Quotient is : " << a / b<< endl;
+    return 0;
+}
