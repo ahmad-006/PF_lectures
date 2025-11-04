4a7c1aca242999b24fcb7277149dcd35acb36d04
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-11-04 09:09:16 +0500

    feat: did the task 3

---
+++ Lab_1/taskThree.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int firstNumber = 20;
+    int secondNumber = 15;
+
+    cout << "The sum of " << firstNumber << " and " << secondNumber << " is: " << firstNumber + secondNumber << endl;
+
+    return 0;
+}
