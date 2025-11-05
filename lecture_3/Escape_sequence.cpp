ae66debfa423911513c078c59dfacfa1198b4b09
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-11-05 09:25:09 +0500

    feat: ignore exe files and vscode config
    - Add .exe and .vscode/ to .gitignore
    - Remove previously tracked .exe files
    - Update Escape_sequence.cpp

--- lecture_3/Escape_sequence.cpp
+++ lecture_3/Escape_sequence.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main() {
     
     //using quotes inside output quotes using escape sequence
-    cout << "Learning to code is an \"essentail\" skill " << endl;
+    cout << "Learning to code is an \"essential\" skill " << endl;
     return 0;
 }
