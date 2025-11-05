3e941177ab4d94d2dedb4af5e35856d19ec29d24
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-11-05 09:07:51 +0500

    feat: used escape sequence to add quotes inside output string

---
+++ lecture_3/Escape_sequence.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    //using quotes inside output quotes using escape sequence
+    cout << "Learning to code is an \"essentail\" skill " << endl;
+    return 0;
+}
