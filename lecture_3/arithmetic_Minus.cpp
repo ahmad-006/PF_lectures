ad501cb515b2c29799cfa899cdb7084500f0da85
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-11-09 23:26:52 +0500

    feat: made a file about minus operator in cpp

---
+++ lecture_3/arithmetic_Minus.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Arithmetic Operators
+
+    // Minus Operator
+    int firstNumber = 4, secondNumber = 3;
+    int difference = firstNumber - secondNumber;
+    cout<< "The difference is " << difference << endl;
+    
+    return 0;
+}
