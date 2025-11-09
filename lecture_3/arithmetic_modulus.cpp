6f7c8cfbef094f3380a49e78018458014d77e6d0
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-11-09 23:28:42 +0500

    feat: made a file about modulus operator in cpp

---
+++ lecture_3/arithmetic_modulus.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Arithmetic Operators
+
+    // Plus Operator
+    int firstNumber = 4, secondNumber = 3;
+    int remainder = firstNumber /secondNumber;
+    cout<< "The remainder is " << remainder << endl;
+    
+    return 0;
+}
