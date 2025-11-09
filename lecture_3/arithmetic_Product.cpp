c950a91e9aa710eb40f871f1cb2c37b374b47d22
Ahmad Aamir <ahmadaamir1806@gmail.com>
2025-11-09 23:27:38 +0500

    feat: made a file about product operator in cpp

---
+++ lecture_3/arithmetic_Product.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Arithmetic Operators
+
+    // Plus Operator
+    int firstNumber = 4, secondNumber = 3;
+    int product = firstNumber * secondNumber;
+    cout<< "The product is " << product << endl;
+    
+    return 0;
+}
